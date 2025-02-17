2506b0b8a4cb9973a64d4a39c8f2487648c2aad3
Atip Kajitamkul <2925772k@student.gla.ac.uk>
2025-02-17 19:47:32 +0000

    Features: Custom API to retrieve CPU data and migrate service to utilise runtime.port

--- cost-model-addon/src/content/index.ts
+++ cost-model-addon/src/content/index.ts
@@ -1,13 +1,17 @@
-import { ContentTrackingHandler } from './tracking-handler';
+import { ContentTrackingHandler } from "./tracking-handler";
 
 const contentTrackingHandler = new ContentTrackingHandler();
 
-browser.runtime.onMessage.addListener(async (message) => {
-  if (message.type === "TRACKING_STATE") {
+
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "TRACKING_STATE" && message.from === "background") {
+    console.log("Received message from background script", message);
     if (message.payload) {
       contentTrackingHandler.enableTracking();
-    } else {
+      sendResponse({ type: "TRACKING_STATE", payload: "Establish port connection", from: "content" });
+    } else { 
       contentTrackingHandler.disableTracking();
+      sendResponse({ type: "TRACKING_STATE", payload: "Disable port connection", from: "content" });
     }
   }
 });

8a72c3cc63c26ec7e5924896b2ea521d53bf8ccb
Atip Kajitamkul <2925772k@student.gla.ac.uk>
2025-02-27 03:04:27 +0000

    Feature: Streamline CPU to the tab & Monitoring feature
    CPU info API now returned windows and threads inside each child process.

--- cost-model-addon/src/background/components/content-component.ts
+++ cost-model-addon/src/background/components/content-component.ts
@@ -1,4 +1,5 @@
 import { MessageType } from '../../common/message.types';
+import { getActiveTab } from '../services/tab-service';
 import { BaseComponent } from './base-component';
 
 export class ContentComponent extends BaseComponent {
@@ -21,5 +22,39 @@ export class ContentComponent extends BaseComponent {
         payload: { state }
       });
     }
-  
+
+    public onClicked(message: any): void {
+      this.mediator.notify(this, {
+        type: MessageType.CPU_USAGE_REQUEST,
+        payload: message
+      });
+    }
+    
+    public async onCPUUsageResponse(CPUInfo: MainProcessInfo): Promise<void> {
+      //  const childrenProcessMap : Map = new Map<ChildProcessInfo["origin"], ChildProcessInfo>();
+       const children = CPUInfo.children.map((child: ChildProcessInfo) => {
+          return {
+            [child.origin]: {
+              cpuCycleCount: child.cpuCycleCount,
+              cpuTime: child.cpuTime,
+              childId: child.childID,
+              memory: child.memory,
+              pid: child.pid,
+              origin: child.origin,
+              // threads: child.threads,
+              // windows: child.windows,
+            }
+          };
+       })
+
+       getActiveTab().then((tab) => {
+        console.log("Active Tab: ", tab?.id);
+       });
+
+       console.log("Children Process Map: ", children);
+
+       const sumChildMemory = CPUInfo.children.reduce((acc: number, child: ChildProcessInfo) => {
+          return acc + child.memory;
+        }, 0);
+    }
 }

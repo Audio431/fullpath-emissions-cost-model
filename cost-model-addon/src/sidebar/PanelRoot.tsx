939742a1d4eb7f1431b00387dfaae2514771396b
Atip Kajitamkul <2925772k@student.gla.ac.uk>
2025-03-06 06:14:14 +0000

    Features: interface v1

--- cost-model-addon/src/sidebar/PanelRoot.tsx
+++ cost-model-addon/src/sidebar/PanelRoot.tsx
@@ -1,9 +1,13 @@
 import * as ReactDOM from 'react-dom/client';
-import SideBar from '../logic/panel';
+import SideBar from './logic/panel';
+import * as React from 'react';
 
 browser.action.onClicked.addListener(() => {
 	browser.sidebarAction.close();
 });
 
 const root = ReactDOM.createRoot(document.getElementById('sidebar-root')!);
-root.render(<SideBar />);
+root.render(
+	<React.StrictMode>
+		<SideBar />
+	</React.StrictMode>);

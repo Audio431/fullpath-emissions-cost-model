8692c9a97125627eb4ca1fcc8c3e333f68663c8e
Atip Kajitamkul <2925772k@student.gla.ac.uk>
2025-03-06 06:36:45 +0000

    Style: clean socket on connect

--- cost-estimation-server/src/websocket.ts
+++ cost-estimation-server/src/websocket.ts
@@ -3,6 +3,7 @@ import { logger } from './logger.js';
 import WebSocket, { WebSocketServer } from 'ws';
 import { AggregationService } from './aggregation-service.js';
 import util from 'util';
+import * as http from 'http';
 
 export const app = express();
 
@@ -10,15 +11,6 @@ export const ws = new WebSocketServer({ noServer: true });
 const clients = new Map<string, WebSocket>();
 const lastMessageMap = new Map<string, string>();
 const aggregationService = new AggregationService();
-
-function isDuplicated(clientId: string, message: string): boolean {
-	if (lastMessageMap.get(clientId) === message) {
-		return true; // It's a duplicate message
-	}
-
-	lastMessageMap.set(clientId, message);
-	return false;
-}
 
 app.use('/api', (req: express.Request, res: express.Response, next) => {
 	logger.info(`HTTP ${req.method} ${req.url}`);
@@ -43,61 +35,108 @@ app.get('/api/start-process', (req: express.Request, res: express.Response): voi
 	res.json({ message: 'WebSocket created' });
 });
 
-ws.on('connection', (ws, request) => {
-	const clientId = new URL(request.url!, `http://${request.headers.host}`).searchParams.get('clientId');
+ws.on('connection', handleConnection);
 
-	if (!clientId) {
-		ws.close();
-		return;
+
+function isDuplicated(clientId: string, message: string): boolean {
+	if (lastMessageMap.get(clientId) === message) {
+		return true; // It's a duplicate message
 	}
 
+	lastMessageMap.set(clientId, message);
+	return false;
+}
+
+async function handleConnection(ws: WebSocket, request: http.IncomingMessage) {
+	const clientId = new URL(request.url!, `http://${request.headers.host}`).searchParams.get('clientId');
+	
+	if (!clientId) {
+	  ws.close();
+	  return;
+	}
+	
 	logger.info(`WebSocket connection established for client: ${clientId}`);
 	ws.send(`Welcome, Client ${clientId}!`);
+	
 	clients.set(clientId, ws);
+	
+	ws.on('message', await createMessageHandler(ws, clientId));
+	
+	ws.on('close', () => {
+	  clients.delete(clientId);
+	  logger.info(`WebSocket connection closed for client: ${clientId}`);
+	});
+}
 
-	ws.on('message', async (message) => {
+// Message handler factory
+async function createMessageHandler(ws: WebSocket, clientId: string) : Promise<(message: WebSocket.Data) => void> {
+	return async (message: WebSocket.Data) => {
 		const messageStr = message.toString();
 
-		if (isDuplicated(clientId, messageStr)) return;
+		if (isDuplicated(clientId, messageStr)) {
+			return;
+		}
 
 		try {
 			const data = JSON.parse(messageStr);
 
-			if (data.type === 'CPU_USAGE') {
+			switch (data.type) {
+				case 'CPU_USAGE':
+					await handleCpuUsage(ws, clientId, data.payload);
+					break;
 
-				await aggregationService.processAggregatedData(data.payload);
-				ws.send(`Received CPU Usage: ${data.payload.cpuUsage}`);
+				case 'NETWORK_DATA':
+					handleNetworkData(ws, clientId, data.payload);
+					break;
 
-			} else if (data.type === 'PREPARE_TO_CLOSE') {
+				case 'PREPARE_TO_CLOSE':
+					await handlePrepareToClose(ws, clientId);
+					break;
 
-				ws.send('Sending aggregated data to client...');
-
-				const aggregatedData = await aggregationService.getAggregatedDataOfEachTab();
-				const co2Emissions = await aggregationService.convertCPUTimeToCO2Emissions();
-
-				ws.send(JSON.stringify([
-					{
-						type: 'AGGREGATED_CPU_USAGE',
-						// payload: aggregatedData
-						// for debug
-						payload: util.inspect(aggregatedData, { showHidden: false, depth: null })
-					}, 
-					{
-						type: 'CO2_EMISSIONS',
-						// payload: co2Emissions
-						// for debug
-						payload: util.inspect(co2Emissions, { showHidden: false, depth: null })
-					}
-				]));
-
-				ws.send('Closing WebSocket connection...');
-				ws.close();
-				clientId && clients.delete(clientId);
-				aggregationService.clearData();
+				default:
+					ws.send(`Unknown message type: ${data.type}`);
 			}
 		} catch (error: any) {
 			logger.warn(`Client ${clientId} sent plain text: "${messageStr}"`);
 			ws.send(`Received Text: "${messageStr}"`);
 		}
-	});
-});
+	};
+}
+
+async function handleCpuUsage(ws: WebSocket, clientId: string, payload: any) {
+	await aggregationService.processAggregatedData(payload);
+	ws.send(`Received CPU Usage: ${payload.cpuUsage}`);
+}
+
+// Handle network data
+async function handleNetworkData(ws: WebSocket, clientId: string, payload: any) {
+	logger.info(util.inspect(`Client ${clientId} sent network usage: ${payload}`));
+	ws.send(`Echo: ${JSON.stringify({ type: 'NETWORK_DATA', payload })}`);
+}
+
+async function handlePrepareToClose(ws: WebSocket, clientId: string) {
+	ws.send('Sending aggregated data to client...');
+
+	const aggregatedData = await aggregationService.getAggregatedDataOfEachTab();
+	const co2Emissions = await aggregationService.convertCPUTimeToCO2Emissions();
+
+	ws.send(JSON.stringify([
+		{
+			type: 'AGGREGATED_CPU_USAGE',
+			payload: util.inspect(aggregatedData, { showHidden: false, depth: null })
+		},
+		{
+			type: 'CO2_EMISSIONS',
+			payload: util.inspect(co2Emissions, { showHidden: false, depth: null })
+		}
+	]));
+
+	ws.send('Closing WebSocket connection...');
+	ws.close();
+
+	if (clientId) {
+		clients.delete(clientId);
+	}
+
+	aggregationService.clearData();
+}

caccdc45dad0418436d8dcb00009b3c13a38149c
Atip Kajitamkul <2925772k@student.gla.ac.uk>
2025-03-25 03:28:38 +0000

    Change url to dest server

--- cost-model-addon/src/background/services/api.ts
+++ cost-model-addon/src/background/services/api.ts
@@ -1,6 +1,7 @@
 import { ApiResponse } from '../../common/api.types';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://fullpath-energyemissions-cost-model.onrender.com/api';
 
 export const api = {
   async startProcess(clientId: string): Promise<ApiResponse<{ message: string }>> {

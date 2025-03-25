6a7bc4ec065f704adb231657d32e4536eb8c8438
Atip Kajitamkul <64627726+Audio431@users.noreply.github.com>
2025-03-25 02:06:25 +0000

    Delete Cost-model-addon directory

--- Cost-model-addon/src/background/background.js
+++
@@ -1,6 +0,0 @@
-browser.runtime.onMessage.addListener((message) => {
-    if (message.data === "start testing performance") {
-        console.log("message received");
-        // startTestingPerformance();
-    }
-});

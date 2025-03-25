4ebbc884be6d4dc5792812b1cf80fcee07836fe1
Atip Kajitamkul <64627726+Audio431@users.noreply.github.com>
2025-03-25 02:07:37 +0000

    Delete Cost-model-addon directory

--- Cost-model-addon/src/devtools/panel/panel.js
+++
@@ -1,6 +0,0 @@
-const perf_button = document.getElementById("perf_button")
-
-perf_button.addEventListener("click", async function() {
-    await browser.runtime.sendMessage({data: "start testing performance"});
-    
-})
